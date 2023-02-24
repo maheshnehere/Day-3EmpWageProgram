5cc03ae6aee0a7fd6e432de11f5794e41b0235fb
Maheshnehere <maheshnehere93@gmail.com>
2023-02-24 17:46:45 +0530

    Added:EmployeeAttendance

---
+++ EmployeeAttendance.java
@@ -0,0 +1,16 @@
+public class EmployeeAttendance {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        // UC 1 = Check Employee is Present or Absent
+        Random random = new Random();
+        boolean num = random.nextBoolean();
+
+        if (num == true) {
+            System.out.println(" Employee is Present");
+        } else {
+            System.out.println(" Employee is Absent");
+        }
+    }
+}
